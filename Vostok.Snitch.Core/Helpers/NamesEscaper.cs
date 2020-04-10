14a6f3b6f3336630748099827445526fe80d6fb7
kungurtsev <kungurtsev@skbkontur.ru>
2020-04-10 15:07:15 +0500

    + NamesEscaper

---
+++ Vostok.Snitch.Core/Helpers/NamesEscaper.cs
@@ -0,0 +1,18 @@
+﻿namespace Vostok.Snitch.Core.Helpers
+{
+    public static class NamesEscaper
+    {
+        public static string Escape(string name)
+        {
+            return name
+                .Replace('.', '_')
+                .Replace('/', '-')
+                .Replace('\\', '-')
+                .Replace(' ', '_')
+                .Replace(':', '_')
+                .Replace('*', '_')
+                .Replace('#', '_')
+                .Replace('~', ':');
+        }
+    }
+}
